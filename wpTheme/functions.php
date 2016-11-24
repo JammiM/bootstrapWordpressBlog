3e85aa146433b60a78670bb6055f7750b4050544
JammiM <jammim19@gmail.com>
2016-11-24 17:32:49 +0000

    Completed the main post loop functionality

--- wpTheme/functions.php
+++ wpTheme/functions.php
@@ -5,6 +5,12 @@
 
   //Theme Support
   function wpb_theme_setup() {
+
+    //adds option to include a thumbnail on a post.
+    add_theme_support('post-thumbnails');
+
+
+
     //Nav Menus
     register_nav_menus(array(
       'primary' => __('Primary Menu')
@@ -13,3 +19,12 @@
   }//wpTheme_setup
 
   add_action('after_setup_theme','wpb_theme_setup');
+
+
+
+  //  Excerpt length Control
+  function set_excerpt_length(){
+    return 30;
+  }//set_excerpt_length
+
+  add_filter('excerpt_length', 'set_excerpt_length' );
